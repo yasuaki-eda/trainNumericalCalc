685f51d01be505b73ed73f784a85017049aca5fb
yasuaki-eda <ryotu0303@gmail.com>
2021-01-18 11:09:55 +0900

    laplace jacobi-method

--- numericalAndSimulation/laplace.py
+++ numericalAndSimulation/laplace.py
@@ -1,7 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import math
 
 
+'''
+ ヤコビ法によるラプラス方程式の反復数値計算
+ @param u : 境界条件を設定した点群(x, y, value). 中央は0で初期化済
+ @param cycle : ループ数
+'''
+def laplace_jacobi_method(u, cycle=1000):
+  h, w = u.shape
+  u_out = np.copy(u)
+  for c in range(cycle):
+    u1 = u_out[:h-2, 1:w-1]
+    u2 = u_out[1:h-1, :w-2]
+    u3 = u_out[1:h-1, 2:w]
+    u4 = u_out[2:h, 1:w-1]
+    u_out[1:h-1, 1:w-1] = (u1+u2+u3+u4)/4
+#    print(u1[10, :10], u1[:10,10])
+  return u_out
 
+
+def test_laplace_jacobi():
+  H, W = 100, 150
+  u = np.zeros((H, W))
+  X = np.array(range(H))
+  Y = np.array(range(W))
+  # 境界条件を設定
+  u[0, :] = np.sin(Y/W*2*math.pi)
+  u[:, 0] = np.cos(X/H*2*math.pi)
+  u[H-1, :] = 1- Y/np.max(Y)
+  u[:, W-1] = 0
+
+  # 反復計算
+  u_out = laplace_jacobi_method(u, cycle=1000)
+
+  # 結果の描画
+  XX, YY = np.meshgrid(X, Y, indexing='ij')
+  fig = plt.figure()
+  ax = Axes3D(fig)
+  ax.plot_surface(XX, YY, u_out, cmap=cm.coolwarm)
+  plt.show()
 
 
 def gauss_v0(A, b):
@@ -63,4 +104,7 @@ def test_gauss_v0():
   gauss_v0(A, b)
 
 if __name__=='__main__':
-  test_gauss_v0()
+#  test_gauss_v0()
+  test_laplace_jacobi()
+
+
